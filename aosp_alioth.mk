c09686fbb8c9a0f3e9ae582e17bcac98f4bd6487
Eidoron1 <eidoronp67@gmail.com>
2022-03-18 04:51:11 +0545

    Lets start the arcanery

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -9,17 +9,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
 TARGET_BOOT_ANIMATION_RES := 108o
-PRODUCT_NAME := nad_alioth
+PRODUCT_NAME := aosp_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
+
+ARCANA_DEVICE := alioth
+ARCANA_MAINTAINER := イタチ
+ARCANA_OFFICIAL := true
+WITH_GAPPS := true
 
 TARGET_SUPPORTS_QUICK_TAP := true
 
